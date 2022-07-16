a88457414c0d253f3249586e4db928c32572ce48
William <William.Jeffery.Knight@protonmail.com>
2022-07-16 13:04:34 -0400

    delete some import in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-// import { Container, Navbar, NavbarBrand } from 'reactstrap';
-// import NucampLogo from './app/assets/img/logo.png';
-// import logo from './logo.svg';
 import Header from './components/Header';
-import { Counter } from './features/counter/Counter';
 import CampsitesList from './features/campsites/CampsitesList.js';
-import { CAMPSITES } from './app/shared/CAMPSITES';
 import Footer from './components/Footer';
 import './App.css';
 
