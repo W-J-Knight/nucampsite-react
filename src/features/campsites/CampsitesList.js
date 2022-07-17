439f57e38c93a103968f7131e8ab28705c9733e7
William <William.Jeffery.Knight@protonmail.com>
2022-07-17 14:33:44 -0400

    campsiteSlice
    2. Exercise: campsitesSlice.js

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,19 +1,19 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () =>{
-    return (
-        <Row>
+    const campsites = selectAllCampsites()
 
-            {CAMPSITES.map((campsite) => {
+    return (
+        <Row className='ms-auto'>
+            {campsites.map((campsite) => {
             return (
                 <Col md='5' className='m-4' key={campsite.id}>
                     <CampsiteCard campsite={campsite} />
                 </Col>
             );
-        })}   
-            
+        })}               
         </Row>
     )
 };
