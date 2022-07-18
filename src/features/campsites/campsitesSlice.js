233dff8ec4afe39208aa106a3b192d31a2fa86d4
William <William.Jeffery.Knight@protonmail.com>
2022-07-17 22:02:24 -0400

    Homepage Display
    5. Exercise: HomePage Display

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -6,7 +6,15 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 }
 
-export const selectRandomCampsite = () => {
-    // return a random campsite
-    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)]
+// export const selectRandomCampsite = () => {
+//     // return a random campsite
+//     return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)]
+// }
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find(campsite => campsite.id === id);
+}
+
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured)
 }
