75c1d824ea642d335d65f4d70229b5d4cc74f2ab
William <William.Jeffery.Knight@protonmail.com>
2022-07-18 08:53:16 -0400

    fix load for http://localhost:3000/directory/0
    by going back

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -12,7 +12,7 @@ export const selectAllCampsites = () => {
 // }
 
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find(campsite => campsite.id === id);
+    return CAMPSITES.find(campsite => campsite.id === parseInt(id));
 }
 
 export const selectFeaturedCampsite = () => {
