c4d2cf7f935b4a6515c4a5e0f8133c05a663dbc2
William <William.Jeffery.Knight@protonmail.com>
2022-07-24 12:21:28 -0400

    4. Exercise: AnimatedDisplayCard finished

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import React from 'react';
-import Header from './components/Header';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import Footer from './components/Footer';
-import AboutPage from './pages/AboutPage';
-import CampsiteDetailPage from './pages/CampsiteDetialPage'
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import React from "react";
+import Header from "./components/Header";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import Footer from "./components/Footer";
+import AboutPage from "./pages/AboutPage";
+import CampsiteDetailPage from "./pages/CampsiteDetialPage";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-        <Routes>
-          <Route path='/' element={<HomePage/>} />   
-          <Route path='contact' element={<ContactPage/>} />
-          <Route path='directory' element={<CampsitesDirectoryPage/>} />  
-          <Route
-                    path='directory/:campsiteId'
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="contact" element={<ContactPage />} />
+                <Route path="directory" element={<CampsitesDirectoryPage />} />
+                <Route
+                    path="directory/:campsiteId"
                     element={<CampsiteDetailPage />}
                 />
-          <Route path='about' element={<AboutPage/>} />
-        </Routes>
-      <Footer />
-    </div>
-  );
+                <Route path="about" element={<AboutPage />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
