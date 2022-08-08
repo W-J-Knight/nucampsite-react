580fed261365a575fb37765bffc60f2f70dc6a5f
William <William.Jeffery.Knight@protonmail.com>
2022-08-08 05:04:45 -0400

    "Error and Loading" https://learn.nucamp.co/mod/book/view.php?id=6046&chapterid=6275

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
@@ -7,9 +9,17 @@ import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import Footer from "./components/Footer";
 import AboutPage from "./pages/AboutPage";
 import CampsiteDetailPage from "./pages/CampsiteDetialPage";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
 import "./App.css";
 
 function App() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+    }, [dispatch]);
+
     return (
         <div className="App">
             <Header />
